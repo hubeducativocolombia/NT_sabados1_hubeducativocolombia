03bce01c6eec223f6de04d53a3d07cd220fe8d65
Yuchico7 <chicac.yuliana@gmail.com>
2026-05-01 22:38:09 -0500

    refactor: mover simulaciones a carpeta utils

--- data/utils/simulacion_instituciones.py
+++ data/utils/simulacion_instituciones.py
@@ -30,7 +30,21 @@ def simular_instituciones(numeroInstituciones):
             "fecha_registro": fechaSimulada.strftime("%Y/%m/%d %H:%M:%S")
         }
 
-        
+        # Ejecutando errores controlados
+        probabilidadError = random.random()
+        if probabilidadError < 0.1:
+            institucion["id_institucion"] = random.choice([None, -1, 0])
+            institucion["nombre_oficial"] = None
+        elif probabilidadError < 0.25:
+            institucion["fecha_registro"] = None
+        elif probabilidadError < 0.4:
+            institucion["nombre_oficial"] = " " + institucion["nombre_oficial"] + " "
+            institucion["naturaleza"] = institucion["naturaleza"].lower()
+        elif probabilidadError < 0.7:
+            institucion["naturaleza"] = random.choice(["N/A", "Desconocida", "", None])
+        elif probabilidadError < 0.9:
+            institucion["nombre_oficial"] = random.choice(["Institución Inválida", "???", "N/A"])
+            institucion["fecha_registro"] = random.choice(["99/99/9999 00:00:00", "00/00/0000 00:00:00", "fecha_invalida"])
 
         instituciones.append(institucion)
 
