60a31cc39a6fde5c58603d59d307f79541d509ca
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 06:40:54 -0500

    fix(backend): icalidadbeneficios package e import

--- backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Repositorio/ICalidadBeneficios.java
+++ backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Repositorio/ICalidadBeneficios.java
@@ -1,6 +1,6 @@
-package com.Cesdeedys.hubEducativoColombia.Repositorio;
+package net.cesde.hubeducativocolombia.repo;
 
-import com.Cesdeedys.hubEducativoColombia.Modelo.MCalidadBeneficios;
+import net.cesde.hubeducativocolombia.model.MCalidadBeneficios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
