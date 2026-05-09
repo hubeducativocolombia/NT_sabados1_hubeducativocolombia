0379935e59d9630a2ca4027f39b6edfab73526f1
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 07:24:12 -0500

    fix(backend): hubeducativocolombiaapplicationtest package y renombrar dir com a test y cesdeedys a cesde y hubeducativocolombia en pascal case a hubeducativocolombia en lower case

--- backend/hubeducativocolombia/src/test/java/net/cesde/hubeducativocolombia/HubEducativoColombiaApplicationTests.java
+++ backend/hubeducativocolombia/src/test/java/net/cesde/hubeducativocolombia/HubEducativoColombiaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Cesdeedys.hubEducativoColombia;
+package net.cesde.hubeducativocolombia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
