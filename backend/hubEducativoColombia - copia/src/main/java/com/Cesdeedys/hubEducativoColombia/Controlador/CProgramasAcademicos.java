1372a15db25e458e6d980103106d5141f85b287b
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 06:26:05 -0500

    fix(backend): cprogramasacademicos package e imports

--- backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Controlador/CProgramasAcademicos.java
+++ backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Controlador/CProgramasAcademicos.java
@@ -1,7 +1,7 @@
-package com.Cesdeedys.hubEducativoColombia.Controlador;
+package net.cesde.hubeducativocolombia.controller;
 
-import com.Cesdeedys.hubEducativoColombia.Modelo.MProgramasAcademicos;
-import com.Cesdeedys.hubEducativoColombia.Servicio.SProgramasAcademicos;
+import net.cesde.hubeducativocolombia.model.MProgramasAcademicos;
+import net.cesde.hubeducativocolombia.service.SProgramasAcademicos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
