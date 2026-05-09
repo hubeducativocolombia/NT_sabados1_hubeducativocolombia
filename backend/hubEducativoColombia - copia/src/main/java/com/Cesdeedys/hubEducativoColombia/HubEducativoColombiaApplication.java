281a05c4954d6a89ede9c9b0d17329aeb40acd9c
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 06:50:46 -0500

    fix(backend): hubeducativocolombiaapplication package

--- backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/HubEducativoColombiaApplication.java
+++ backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/HubEducativoColombiaApplication.java
@@ -1,4 +1,4 @@
-package com.Cesdeedys.hubEducativoColombia;
+package net.cesde.hubeducativocolombia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
