71e0d6cbc34391e5ac9005b53d8857bd761a0e61
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 06:48:12 -0500

    fix(backend): scalidadbeneficios package e imports

--- backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Servicio/SCalidadBeneficios.java
+++ backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Servicio/SCalidadBeneficios.java
@@ -1,7 +1,7 @@
-package com.Cesdeedys.hubEducativoColombia.Servicio;
+package net.cesde.hubeducativocolombia.service;
 
-import com.Cesdeedys.hubEducativoColombia.Modelo.MCalidadBeneficios;
-import com.Cesdeedys.hubEducativoColombia.Repositorio.ICalidadBeneficios;
+import net.cesde.hubeducativocolombia.model.MCalidadBeneficios;
+import net.cesde.hubeducativocolombia.repo.ICalidadBeneficios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
