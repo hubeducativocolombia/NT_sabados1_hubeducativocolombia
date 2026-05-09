defc6ba995b17dbc45f028c517287dc6f02d90e8
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 06:47:26 -0500

    fix(backend): sdetallesoperacion package e imports

--- backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Servicio/SDetallesOperacion.java
+++ backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Servicio/SDetallesOperacion.java
@@ -1,7 +1,7 @@
-package com.Cesdeedys.hubEducativoColombia.Servicio;
+package net.cesde.hubeducativocolombia.service;
 
-import com.Cesdeedys.hubEducativoColombia.Modelo.MDetallesOperacion;
-import com.Cesdeedys.hubEducativoColombia.Repositorio.IDetallesOperacion;
+import net.cesde.hubeducativocolombia.model.MDetallesOperacion;
+import net.cesde.hubeducativocolombia.repo.IDetallesOperacion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
