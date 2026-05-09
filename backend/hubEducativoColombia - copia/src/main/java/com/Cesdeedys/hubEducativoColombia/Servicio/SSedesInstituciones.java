6e824413b87620120f99df975f203587e3ed2dbb
Edwin Rios Sanchez <edwinrios84@gmail.com>
2026-05-09 06:45:12 -0500

    fix(backend): ssedesinstituciones package e imports

--- backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Servicio/SSedesInstituciones.java
+++ backend/hubEducativoColombia - copia/src/main/java/com/Cesdeedys/hubEducativoColombia/Servicio/SSedesInstituciones.java
@@ -1,7 +1,7 @@
-package com.Cesdeedys.hubEducativoColombia.Servicio;
+package net.cesde.hubeducativocolombia.service;
 
-import com.Cesdeedys.hubEducativoColombia.Modelo.MSedesIntituciones;
-import com.Cesdeedys.hubEducativoColombia.Repositorio.ISedesInstituciones;
+import net.cesde.hubeducativocolombia.model.MSedesIntituciones;
+import net.cesde.hubeducativocolombia.repo.ISedesInstituciones;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
