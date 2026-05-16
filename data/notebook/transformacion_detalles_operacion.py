b6f7e71890f996d8e3fbd1e1588768c4bc53ee50
Diana Zapata <nana.ortega71@gmail.com>
2026-05-16 15:04:09 -0500

    feat se anexa consumo y transformacion a NT

--- data/notebook/transformacion_detalles_operacion.py
+++ data/notebook/transformacion_detalles_operacion.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def transform_datos(data_frame_limpio):
-    filtro=data_frame_limpio.query("id_programa=='Ingenieria de Sistemas'")
-    agrupacion=filtro.groupby("fecha")["id"].count().reset_index(name="conteo")
+    filtro=data_frame_limpio.query("id_detallesOperacion=='Ingenieria de Sistemas'")
+    agrupacion=filtro.groupby("fechaActualizacion")["id_programa"].count().reset_index(name="conteo")
 
-    filtro2=data_frame_limpio.query("costo>=250000")
-    agrupacion2=filtro2.groupby("id_programa")["costo"].sum().reset_index(name="sumatoria")
+    filtro2=data_frame_limpio.query("costo_semestre>=2500000")
+    agrupacion2=filtro2.groupby("id_programa")["costo_semestre"].sum().reset_index(name="sumatoria")
 
     transformacion_resumen={
         "conteoIngenieriaDeSistemasPorFecha":agrupacion,
