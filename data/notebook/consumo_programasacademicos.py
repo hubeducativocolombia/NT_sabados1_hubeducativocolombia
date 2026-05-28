5640249754050ef57fad3b635a18ee005d815165
hubeducativocolombia <hubeducativocolombia@gmail.com>
2026-05-27 23:03:17 -0500

    fix: actualizar URLs archivos consumo todas las tablas

--- data/notebook/consumo_programasacademicos.py
+++ data/notebook/consumo_programasacademicos.py
@@ -2,7 +2,7 @@ import requests
 
 
 def consumir_programasacademicos():
-    url = "http://localhost:8080/api/programas"
+    url = "http://localhost:8080/programas"
     respuesta = requests.get(url)
     respuesta.raise_for_status()
     datos = respuesta.json()
--- data/notebook/consumo_sedesinstituciones.py
+++ data/notebook/consumo_sedesinstituciones.py
@@ -2,7 +2,7 @@ import requests
 
 
 def consumir_sedesinstituciones():
-    url = "http://localhost:8080/api/sedes"
+    url = "http://localhost:8080/sedes"
     respuesta = requests.get(url)
     respuesta.raise_for_status()
     datos = respuesta.json()
